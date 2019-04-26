3ca68c9ba7beba91cd250edbd1243f2db91ec886
J. Tower <jonathantower@gmail.com>
2019-04-25 21:30:31 -0400

    Adding code files, updating slides

---
+++ code/Middleware/Middleware/ResponseTimeMiddlewareExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Middleware.Middleware
+{
+    public static class ResponseTimeMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseResponseTime(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ResponseTimeMiddleware>();
+        }
+    }
+}
---
+++ code/TagHelpers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCorePrimeTime.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
